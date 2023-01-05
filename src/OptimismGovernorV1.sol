eab6e287bd9a820a1be18710d13b65fc3e41d58c
w1nt3r-eth <w1nt3r.eth@gmail.com>
2023-01-04 18:07:56 -0800

    Initial implementation of Optimism governance

---
+++ src/OptimismGovernorV1.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovernorUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
+import {GovernorCountingSimpleUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
+import {GovernorVotesQuorumFractionUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
+import {GovernorVotesUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesUpgradeable.sol";
+import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+import {TimersUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/TimersUpgradeable.sol";
+
+contract OptimismGovernorV1 is
+    GovernorCountingSimpleUpgradeable,
+    GovernorVotesUpgradeable,
+    GovernorVotesQuorumFractionUpgradeable
+{
+    using TimersUpgradeable for TimersUpgradeable.BlockNumber;
+
+    event ProposalDeadlineUpdated(uint256 proposalId, uint64 deadline);
+
+    address public manager;
+
+    function __OptimismGovernorV1_init(IVotesUpgradeable _votingToken, address _manager) internal onlyInitializing {
+        __Governor_init("Optimism");
+        __GovernorVotes_init(_votingToken);
+        __GovernorVotesQuorumFraction_init(10); // TODO: Quorum value
+        manager = _manager;
+    }
+
+    function votingDelay() public pure override returns (uint256) {
+        // TODO: Voting delay value
+        return 6575; // 1 day, in blocks
+    }
+
+    function votingPeriod() public pure override returns (uint256) {
+        // TODO: Voting period value
+        return 46027; // 1 week, in blocks
+    }
+
+    function _execute(
+        uint256, /* proposalId */
+        address[] memory, /* targets */
+        uint256[] memory, /* values */
+        bytes[] memory, /* calldatas */
+        bytes32 /*descriptionHash*/
+    ) internal view override {
+        require(msg.sender == manager, "Only the manager can execute");
+
+        // Execution is skipped
+    }
+
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public virtual override returns (uint256) {
+        require(msg.sender == manager, "Only the manager can propose");
+
+        return super.propose(targets, values, calldatas, description);
+    }
+
+    function updateProposalDeadline(uint256 proposalId, uint64 deadline) public {
+        require(msg.sender == manager, "Only the manager can update the proposal deadline");
+
+        // TODO: Limit when this can be updated
+        _proposals[proposalId].voteEnd.setDeadline(deadline);
+        emit ProposalDeadlineUpdated(proposalId, deadline);
+    }
+}
