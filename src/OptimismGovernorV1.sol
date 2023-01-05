a9a00d32cb17062178b5837413161cb6913c89d4
w1nt3r-eth <w1nt3r.eth@gmail.com>
2023-01-05 11:14:43 -0800

    Use GovernorSettingsUpgradeable extension

--- src/OptimismGovernorV1.sol
+++ src/OptimismGovernorV1.sol
@@ -8,6 +8,8 @@ import {GovernorVotesQuorumFractionUpgradeable} from
     "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
 import {GovernorVotesUpgradeable} from
     "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesUpgradeable.sol";
+import {GovernorSettingsUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorSettingsUpgradeable.sol";
 import {IVotesUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
 import {TimersUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/TimersUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -16,7 +18,8 @@ contract OptimismGovernorV1 is
     Initializable,
     GovernorCountingSimpleUpgradeable,
     GovernorVotesUpgradeable,
-    GovernorVotesQuorumFractionUpgradeable
+    GovernorVotesQuorumFractionUpgradeable,
+    GovernorSettingsUpgradeable
 {
     using TimersUpgradeable for TimersUpgradeable.BlockNumber;
 
@@ -24,23 +27,19 @@ contract OptimismGovernorV1 is
 
     address public manager;
 
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only the manager can call this function");
+        _;
+    }
+
     function initialize(IVotesUpgradeable _votingToken, address _manager) public initializer {
         __Governor_init("Optimism");
         __GovernorCountingSimple_init();
         __GovernorVotes_init(_votingToken);
-        __GovernorVotesQuorumFraction_init(10); // TODO: Quorum value
+        __GovernorVotesQuorumFraction_init(10);
+        __GovernorSettings_init(6575, 46027, 1); // TODO: Voting delay, voting period, and proposal threshold values
 
         manager = _manager;
-    }
-
-    function votingDelay() public pure override returns (uint256) {
-        // TODO: Voting delay value
-        return 6575; // 1 day, in blocks
-    }
-
-    function votingPeriod() public pure override returns (uint256) {
-        // TODO: Voting period value
-        return 46027; // 1 week, in blocks
     }
 
     function _execute(
@@ -49,9 +48,7 @@ contract OptimismGovernorV1 is
         uint256[] memory, /* values */
         bytes[] memory, /* calldatas */
         bytes32 /*descriptionHash*/
-    ) internal view override {
-        require(msg.sender == manager, "Only the manager can execute");
-
+    ) internal view override onlyManager {
         // Execution is skipped
     }
 
@@ -60,17 +57,37 @@ contract OptimismGovernorV1 is
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public virtual override returns (uint256) {
-        require(msg.sender == manager, "Only the manager can propose");
-
+    ) public override onlyManager returns (uint256) {
         return super.propose(targets, values, calldatas, description);
     }
 
-    function updateProposalDeadline(uint256 proposalId, uint64 deadline) public {
-        require(msg.sender == manager, "Only the manager can update the proposal deadline");
+    function proposalThreshold()
+        public
+        view
+        override (GovernorSettingsUpgradeable, GovernorUpgradeable)
+        returns (uint256)
+    {
+        return GovernorSettingsUpgradeable.proposalThreshold();
+    }
 
-        // TODO: Limit when this can be updated
+    function setProposalDeadline(uint256 proposalId, uint64 deadline) public onlyManager {
         _proposals[proposalId].voteEnd.setDeadline(deadline);
         emit ProposalDeadlineUpdated(proposalId, deadline);
+    }
+
+    function setVotingDelay(uint256 newVotingDelay) public override onlyManager {
+        _setVotingDelay(newVotingDelay);
+    }
+
+    function setVotingPeriod(uint256 newVotingPeriod) public override onlyManager {
+        _setVotingPeriod(newVotingPeriod);
+    }
+
+    function setProposalThreshold(uint256 newProposalThreshold) public override onlyManager {
+        _setProposalThreshold(newProposalThreshold);
+    }
+
+    function updateQuorumNumerator(uint256 newQuorumNumerator) external override onlyManager {
+        _updateQuorumNumerator(newQuorumNumerator);
     }
 }
