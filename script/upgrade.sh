5fe866c24ddeb284c56b42877c0a317eab5e3ebd
jacopo <39241410+jjranalli@users.noreply.github.com>
2023-04-11 20:04:53 +0200

    feat: add OptimismGovernorV4 (#1)
    * forge install: forge-std
    
    v1.5.2
    
    * wip
    
    * forge install: openzeppelin-contracts
    
    v4.8.2
    
    * forge install: openzeppelin-contracts-upgradeable
    
    v4.8.2
    
    * nit: typo
    
    * feat: add OptimismGovernorV4
    
    * feat: add OptimismGovernorV4 upgrade test
    
    * feat: add OptimismGovernorV4 upgrade script
    
    * fix: change onlyManager to a reinitializer
    
    * feat: test refactor
    
    * fix: rename initializer function
    
    * fix: added revert if state doesn't correspond
    
    * nit: formatting
    
    * feat: added upgradeToAndCall to deploy script
    
    * nit: comment
    
    * fix: remove upgradeToAndCall in deploy script
    
    * updated upgrade.sh

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -4,7 +4,7 @@ set -o allexport
 source .env
 set +o allexport
 
-forge script script/UpgradeOptimismGovernorToV3.s.sol -vvvv \
+forge script script/UpgradeOptimismGovernorToV4.s.sol -vvvv \
   --fork-url https://mainnet.optimism.io \
   --broadcast \
   --verify \
