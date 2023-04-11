ceb0a2e99e4070cfe0697dde36b0ce2bbd9dc766
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-11 18:48:34 +0200

    nit: formatting

--- test/OptimismGovernorV4.t.sol
+++ test/OptimismGovernorV4.t.sol
@@ -6,16 +6,19 @@ import {OptimismGovernorV4} from "../src/OptimismGovernorV4.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract OptimismGovernorV4UpgradeTest is Test {
-    TransparentUpgradeableProxy internal constant proxy = TransparentUpgradeableProxy(payable(0xcDF27F107725988f2261Ce2256bDfCdE8B382B10));
+    TransparentUpgradeableProxy internal constant proxy =
+        TransparentUpgradeableProxy(payable(0xcDF27F107725988f2261Ce2256bDfCdE8B382B10));
     OptimismGovernorV4 internal governor = OptimismGovernorV4(payable(address(proxy)));
-    address internal constant admin = 
-0x2501c477D0A35545a387Aa4A3EEe4292A9a8B3F0;
+    address internal constant admin = 0x2501c477D0A35545a387Aa4A3EEe4292A9a8B3F0;
     address internal constant manager = 0xE4553b743E74dA3424Ac51f8C1E586fd43aE226F;
-    
-    uint256 internal constant proposalIdDelegateSuspension = 27878184270712708211495755831534918916136653803154031118511283847257927730426;
-    uint256 internal constant proposalIdBedrock = 114732572201709734114347859370226754519763657304898989580338326275038680037913;
-    uint256 internal constant proposalIdTestVote3 = 103606400798595803012644966342403441743733355496979747669804254618774477345292;
-        
+
+    uint256 internal constant proposalIdDelegateSuspension =
+        27878184270712708211495755831534918916136653803154031118511283847257927730426;
+    uint256 internal constant proposalIdBedrock =
+        114732572201709734114347859370226754519763657304898989580338326275038680037913;
+    uint256 internal constant proposalIdTestVote3 =
+        103606400798595803012644966342403441743733355496979747669804254618774477345292;
+
     OptimismGovernorV4 internal implementation;
 
     function setUp() public {
@@ -62,7 +65,7 @@ contract OptimismGovernorV4UpgradeTest is Test {
         vm.prank(admin);
         proxy.upgradeToAndCall(address(implementation), abi.encodeWithSignature("_correctQuorumForBlock83241938()"));
 
-        vm.expectRevert('Initializable: contract is already initialized');
+        vm.expectRevert("Initializable: contract is already initialized");
         governor._correctQuorumForBlock83241938();
     }
 }
