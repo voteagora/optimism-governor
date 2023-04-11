f7459d998b5e2c62c585b3e8264d029fd4b6eaf3
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-11 18:51:08 +0200

    feat: added upgradeToAndCall to deploy script

--- script/UpgradeOptimismGovernorToV4.s.sol
+++ script/UpgradeOptimismGovernorToV4.s.sol
@@ -4,15 +4,25 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import {OptimismGovernorV4} from "../src/OptimismGovernorV4.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract UpgradeOptimismGovernorToV4Script is Script {
+    TransparentUpgradeableProxy internal constant proxy =
+        TransparentUpgradeableProxy(payable(0xcDF27F107725988f2261Ce2256bDfCdE8B382B10));
+
     function run() public {
         address deployer = vm.rememberKey(vm.envUint("DEPLOYER_KEY"));
 
         vm.startBroadcast(deployer);
+        
         OptimismGovernorV4 implementation = new OptimismGovernorV4();
-        vm.stopBroadcast();
 
         console.log("OptimismGovernorV4 deployed at", address(implementation));
+
+        proxy.upgradeToAndCall(address(implementation), abi.encodeWithSignature("_correctQuorumForBlock83241938()"));
+        
+        console.log("OptimismGovernor upgraded to V4");
+        
+        vm.stopBroadcast();
     }
 }
