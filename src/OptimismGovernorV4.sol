2048fcbb19ac4c7548bcee5465227b3c636569c8
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-04-11 18:26:20 +0200

    fix: change onlyManager to a reinitializer

--- src/OptimismGovernorV4.sol
+++ src/OptimismGovernorV4.sol
@@ -5,7 +5,7 @@ import {OptimismGovernorV3} from "./OptimismGovernorV3.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CheckpointsUpgradeable.sol";
 
 contract OptimismGovernorV4 is OptimismGovernorV3 {
-    function _initializeCheckpoint() public onlyManager {
+    function _initializeCheckpoint() public reinitializer(2) {
         _quorumNumeratorHistory._checkpoints[3] = 
                 CheckpointsUpgradeable.Checkpoint({_blockNumber: 83241938, _value: 149})
             ;
