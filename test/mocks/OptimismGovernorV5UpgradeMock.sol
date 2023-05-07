8de69b2751122efd3a2cafceaa857150545faf1b
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-05-07 16:47:00 +0200

    rename: upgrade mocks

--- test/mocks/OptimismGovernorV5UpgradeMock.sol
+++ test/mocks/OptimismGovernorV5UpgradeMock.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {OptimismGovernorV4} from "../../src/OptimismGovernorV4.sol";
+import {OptimismGovernorV5} from "../../src/OptimismGovernorV5.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 // Mock UUPSUpgradeable to test upgradeability
-contract OptimismGovernorV4Mock is OptimismGovernorV4, UUPSUpgradeable {
+contract OptimismGovernorV5UpgradeMock is OptimismGovernorV5, UUPSUpgradeable {
     function _authorizeUpgrade(address newImplementation) internal override {}
 }
