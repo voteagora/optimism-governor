164a7d8e64380c9d0af194b2b8c0d7ab1c6ce3d5
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-05-07 16:47:20 +0200

    add: governorV5 mock

--- test/mocks/OptimismGovernorV5Mock.sol
+++ test/mocks/OptimismGovernorV5Mock.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.13;
 
 import {OptimismGovernorV5} from "../../src/OptimismGovernorV5.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-// Mock UUPSUpgradeable to test upgradeability
-contract OptimismGovernorV5Mock is OptimismGovernorV5, UUPSUpgradeable {
-    function _authorizeUpgrade(address newImplementation) internal override {}
+// Expose internal functions for testing
+contract OptimismGovernorV5Mock is OptimismGovernorV5 {
+    function quorumReached(uint256 proposalId) public view returns (bool) {
+        return _quorumReached(proposalId);
+    }
+
+    function voteSucceeded(uint256 proposalId) public view returns (bool) {
+        return _voteSucceeded(proposalId);
+    }
 }
