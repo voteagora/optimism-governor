b20a44db65998d18c7cf500216a82d3f1cf21cbb
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2024-01-08 20:39:56 +0100

    nit: script cleanup

--- script/UpgradeOptimismGovernorToV6.s.sol
+++ script/UpgradeOptimismGovernorToV6.s.sol
@@ -12,28 +12,43 @@ import {ProposalTypesConfigurator} from "src/ProposalTypesConfigurator.sol";
 import {IOptimismGovernor} from "src/interfaces/IOptimismGovernor.sol";
 import {AlligatorOPV5} from "src/alligator/AlligatorOP_V5.sol";
 import {ApprovalVotingModule} from "src/modules/ApprovalVotingModule.sol";
+import {OptimisticModule_SocialSignalling} from "src/modules/OptimisticModule.sol";
 
 contract UpgradeOptimismGovernorToV6Script is Script {
     // Test environment addresses
     TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(0x6E17cdef2F7c1598AD9DfA9A8acCF84B1303f43f));
     ProposalTypesConfigurator proposalTypesConfigurator =
         ProposalTypesConfigurator(payable(0x54c943f19c2E983926E2d8c060eF3a956a653aA7));
-    ApprovalVotingModule module = ApprovalVotingModule(0xdd0229D72a414DC821DEc66f3Cc4eF6dB2C7b7df);
+    ApprovalVotingModule approvalModule = ApprovalVotingModule(0xdd0229D72a414DC821DEc66f3Cc4eF6dB2C7b7df);
+    OptimisticModule_SocialSignalling optimisticSocialModule =
+        OptimisticModule_SocialSignalling(0x27964c5f4F389B8399036e1076d84c6984576C33);
     VotableSupplyOracle votableSupplyOracle = VotableSupplyOracle(0x1b7CA7437748375302bAA8954A2447fC3FBE44CC);
     AlligatorOPV5 alligatorV5Impl = AlligatorOPV5(0xD89eb37D3e643aab97258C62BcF704CD00761af6);
     AlligatorOPV5 alligatorV5 = AlligatorOPV5(0x7f08F3095530B67CdF8466B7a923607944136Df0);
 
+    // Prod environment addresses
+    // TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(0xcDF27F107725988f2261Ce2256bDfCdE8B382B10));
+    // ProposalTypesConfigurator proposalTypesConfigurator =
+    //     ProposalTypesConfigurator(payable(0x54c943f19c2E983926E2d8c060eF3a956a653aA7));
+    // ApprovalVotingModule approvalModule = ApprovalVotingModule(0xdd0229D72a414DC821DEc66f3Cc4eF6dB2C7b7df);
+    // OptimisticModule_SocialSignalling optimisticSocialModule =
+    //     OptimisticModule_SocialSignalling(0x27964c5f4F389B8399036e1076d84c6984576C33);
+    // VotableSupplyOracle votableSupplyOracle = VotableSupplyOracle(0x1b7CA7437748375302bAA8954A2447fC3FBE44CC);
+    // AlligatorOPV5 alligatorV5Impl = AlligatorOPV5(0xA2Cf0f99bA37cCCB9A9FAE45D95D2064190075a3);
+    // AlligatorOPV5 alligatorV5 = AlligatorOPV5(0x7f08F3095530B67CdF8466B7a923607944136Df0);
+
     function run() public 
     // returns (
-    //     OptimismGovernorV6_Manageable implementation,
-    //     VotableSupplyOracle votableSupplyOracle,
-    //     ProposalTypesConfigurator proposalTypesConfigurator,
+    //     OptimismGovernorV6 implementation
+    // VotableSupplyOracle votableSupplyOracle,
+    // ProposalTypesConfigurator proposalTypesConfigurator
     //     AlligatorOPV5 alligator,
-    //     ApprovalVotingModule module
+    //     ApprovalVotingModule approvalModule
+    // OptimisticModule_SocialSignalling optimisticSocialModule
     // )
     {
-        // address deployer = vm.rememberKey(vm.envUint("DEPLOYER_KEY"));
-        address deployer = vm.rememberKey(vm.envUint("MANAGER_KEY"));
+        address deployer = vm.rememberKey(vm.envUint("DEPLOYER_KEY"));
+        // address deployer = vm.rememberKey(vm.envUint("MANAGER_KEY"));
 
         vm.startBroadcast(deployer);
         // Pt. 1 Deploy contracts
@@ -41,12 +56,23 @@ contract UpgradeOptimismGovernorToV6Script is Script {
         // proposalTypesConfigurator = new ProposalTypesConfigurator(IOptimismGovernor(address(proxy)));
         // alligator = new AlligatorOPV5();
 
-        // Pt. 2 (opt.) Approve module -- MANAGER
-        // module = new ApprovalVotingModule();
+        // Pt. 2 (opt.) Approve approvalModule approval -- MANAGER
+        // approvalModule = new ApprovalVotingModule();
         // address prevManager = OptimismGovernorV5(payable(proxy)).manager();
         // OptimismGovernorV6_Manageable(payable(proxy))._setManager(deployer);
-        // OptimismGovernorV5(payable(proxy)).setModuleApproval(0xdd0229D72a414DC821DEc66f3Cc4eF6dB2C7b7df, true);
+        // OptimismGovernorV5(payable(proxy)).setModuleApproval(approvalModule, true);
         // OptimismGovernorV6_Manageable(payable(proxy))._setManager(prevManager);
+
+        // Pt. 2 (opt.) Approve module optimistic -- MANAGER
+        // optimisticSocialModule = new OptimisticModule_SocialSignalling();
+        // address prevManager = OptimismGovernorV5(payable(proxy)).manager();
+        // if (prevManager != deployer) {
+        //     OptimismGovernorV6_Manageable(payable(proxy))._setManager(deployer);
+        // }
+        // OptimismGovernorV5(payable(proxy)).setModuleApproval(address(optimisticSocialModule), true);
+        // if (prevManager != deployer) {
+        //     OptimismGovernorV6_Manageable(payable(proxy))._setManager(prevManager);
+        // }
 
         // Pt. 3 Set proposal types -- MANAGER
         // address prevManager = OptimismGovernorV5(payable(proxy)).manager();
@@ -59,7 +85,7 @@ contract UpgradeOptimismGovernorToV6Script is Script {
         // (only on test env) Set correct governor address in `Alligator`
 
         // Pt. 5 Upgrade governor
-        // implementation = new OptimismGovernorV6_Manageable();
+        // implementation = new OptimismGovernorV6();
         // proxy.upgradeTo(address(implementation));
 
         vm.stopBroadcast();
