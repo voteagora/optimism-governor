a15ee506c7ad3561126a87e061f3a14215cb9cfa
Cory Dickson <cory.dickson@protonmail.com>
2025-04-16 16:24:53 -0400

    Use proxy admin for deployment

--- script/deploy/DeployBase.s.sol
+++ script/deploy/DeployBase.s.sol
@@ -9,7 +9,6 @@ import {GovernanceToken} from "src/GovernanceToken.sol";
 import {ApprovalVotingModule} from "src/modules/ApprovalVotingModule.sol";
 import {OptimisticModule_SocialSignalling} from "src/modules/OptimisticModule.sol";
 import {AlligatorOP} from "src/alligator/AlligatorOP.sol";
-import {AlligatorOP} from "src/alligator/AlligatorOP.sol";
 import {IVotingToken} from "src/interfaces/IVotingToken.sol";
 import {IVotableSupplyOracle} from "src/interfaces/IVotableSupplyOracle.sol";
 
@@ -17,8 +16,10 @@ import {Timelock, TimelockControllerUpgradeable} from "test/mocks/TimelockMock.s
 
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 abstract contract DeployBase is Script {
+    ProxyAdmin proxyAdmin;
     GovernanceToken token;
     VotableSupplyOracle votableSupplyOracle;
     ProposalTypesConfigurator proposalTypesConfigurator;
@@ -37,10 +38,11 @@ abstract contract DeployBase is Script {
         // Read caller information.
         (, address deployer,) = vm.readCallers();
 
-        address proxyAdmin = deployer; // Semantics
-
         // Deploy Token
         token = new GovernanceToken();
+
+        // Deploy ProxyAdmin
+        proxyAdmin = new ProxyAdmin();
 
         // Deploy Oracle
         votableSupplyOracle = new VotableSupplyOracle(deployer, 0);
@@ -60,7 +62,7 @@ abstract contract DeployBase is Script {
         );
 
         // Deploy timelock
-        timelock = Timelock(payable(new TransparentUpgradeableProxy(address(new Timelock()), governorManager, "")));
+        timelock = Timelock(payable(new TransparentUpgradeableProxy(address(new Timelock()), address(proxyAdmin), "")));
 
         // Deploy governor impl
         address governorImplementation = address(new OptimismGovernor());
@@ -69,7 +71,7 @@ abstract contract DeployBase is Script {
         address governorProxy = address(
             new TransparentUpgradeableProxy(
                 governorImplementation,
-                proxyAdmin,
+                address(proxyAdmin),
                 abi.encodeCall(
                     OptimismGovernor.initialize,
                     (
@@ -94,16 +96,33 @@ abstract contract DeployBase is Script {
         OptimisticModule_SocialSignalling optimisticModule = new OptimisticModule_SocialSignalling(address(governor));
 
         timelock.initialize(2 days, governorProxy, governorManager);
-        // governor.setModuleApproval(address(approvalModule), true);
-        // governor.setModuleApproval(address(optimisticModule), true);
+        governor.setModuleApproval(address(approvalModule), true);
+        governor.setModuleApproval(address(optimisticModule), true);
 
-        // proposalTypesConfigurator.setProposalType(0, 3_000, 5_000, "Default", "Lorem Ipsum", address(0));
-        // proposalTypesConfigurator.setProposalType(1, 5_000, 7_000, "Alt - Approval", "Lorem Ipsum", address(approvalModule));
-        // proposalTypesConfigurator.setProposalType(2, 0, 0, "Optimistic", "Lorem Ipsum", address(optimisticModule));
+        proposalTypesConfigurator.setProposalType(0, 3_000, 5_000, "Default", "Lorem Ipsum", address(0));
+        proposalTypesConfigurator.setProposalType(1, 5_000, 7_000, "Alt - Approval", "Lorem Ipsum", address(approvalModule));
+        proposalTypesConfigurator.setProposalType(2, 0, 0, "Optimistic", "Lorem Ipsum", address(optimisticModule));
 
         // Mint and set total supply
-        // token.mint ...
-        // votableSupplyOracle._updateVotableSupply(token.totalSupply());
+         address[11] memory holders = [
+            0x1d671d1B191323A38490972D58354971E5c1cd2A,
+            0x648BFC4dB7e43e799a84d0f607aF0b4298F932DB,
+            0xab499F5784Bc91AB9452a69915959AF20326D159,
+            0xFdFC6E1BbEc01288447222fC8F1AEE55a7C72b7B,
+            0xAce36342FfB271DFB8a844e028A0ead5aD51af20,
+            0xC950b9F32259860f4731D318CB5a28B2dB892f88,
+            0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266,
+            0x603F5A389d893624A648424aD58a33205F8fC59c,
+            0xcC0B26236AFa80673b0859312a7eC16d2b72C1ea,
+            0x3D1F084A0580d48db8108E2a3260b699f9343833,
+            deployer
+        ];
+
+        for (uint8 i = 0; i < holders.length; i++) {
+            token.mint(holders[i], 100_000 ether);
+        }
+
+        votableSupplyOracle._updateVotableSupply(token.totalSupply());
 
         vm.stopBroadcast();
     }
--- script/deploy/DeploySepolia.s.sol
+++ script/deploy/DeploySepolia.s.sol
@@ -6,7 +6,7 @@ import "forge-std/Script.sol";
 import {DeployBase} from "script/deploy/DeployBase.s.sol";
 
 contract DeploySepolia is DeployBase {
-    constructor() DeployBase(0x648BFC4dB7e43e799a84d0f607aF0b4298F932DB) {}
+    constructor() DeployBase(0xA622279f76ddbed4f2CC986c09244262Dba8f4Ba) {}
 
     function run() external {
         setup();
